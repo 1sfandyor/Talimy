e1fbe0499c3aa14a9dacf98702d75353632c5fb5
1sfandyor <isfandiyor06112000@gmail.com>
2026-02-26 16:24:04 +0300

    fix(api): import auth module in audit module for guards

--- apps/api/src/modules/audit/audit.module.ts
+++ apps/api/src/modules/audit/audit.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common"
 import { APP_INTERCEPTOR } from "@nestjs/core"
 
+import { AuthModule } from "../auth/auth.module"
 import { AuditController } from "./audit.controller"
 import { AuditInterceptor } from "./audit.interceptor"
 import { AuditService } from "./audit.service"
 
 @Module({
+  imports: [AuthModule],
   controllers: [AuditController],
   providers: [
     AuditService,
